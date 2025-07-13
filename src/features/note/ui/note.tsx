c438ac0be9cd081eb2ecdbbf65bb038d0e6aaab1
massssonya <vladsozonov01@gmail.com>
2025-07-13 23:44:17 +0300

    feat: add uikit component dropdown

--- src/features/note/ui/note.tsx
+++ src/features/note/ui/note.tsx
@@ -1,15 +1,15 @@
 import clsx from "clsx";
 import {
 	Archive,
-	// MoreVertical,
+	MoreVertical,
 	Image,
 	UserPlus,
 	Bell,
-	Smile,
-	X
+	Smile
 } from "@geist-ui/icons";
 import type { Note } from "@/shared/types/note";
 import { useDeleteNote } from "../model";
+import { DropdownMenu } from "@/shared/ui";
 
 type Props = {
 	note: Note;
@@ -18,10 +18,19 @@ type Props = {
 
 export function Note({ note, className }: Props) {
 	const { deleteNote } = useDeleteNote(note.id);
+
+	const items = [
+		{
+			label: "Удалить заметку",
+			onClick: () => deleteNote()
+		}
+	];
+
 	return (
 		<div
+			tabIndex={0}
 			className={clsx(
-				"bg-white border border-gray-300 rounded-xl px-4 py-3 hover:shadow-md hover:border-white transition-all duration-300 ease-in-out group relative",
+				"bg-white border border-gray-300 rounded-xl px-4 py-3 hover:shadow-md hover:border-white transition-all duration-300 ease-in-out group",
 				className
 			)}
 		>
@@ -30,13 +39,24 @@ export function Note({ note, className }: Props) {
 			)}
 			<p className="text-sm text-gray-800 whitespace-pre-line">{note.text}</p>
 
-			<footer className="mt-3 flex justify-between text-gray-500 opacity-0 group-hover:opacity-100 transition-opacity duration-300 ease-in-out">
+			<footer className="mt-3 flex justify-between text-gray-500 opacity-0 group-hover:opacity-100 transition-opacity duration-300 ease-in-out group-focus:opacity-100">
 				<Smile className="button-hover" />
 				<Bell className="button-hover" />
 				<UserPlus className="button-hover" />
 				<Image className="button-hover" />
 				<Archive className="button-hover" />
-				<X className="button-hover" onClick={deleteNote} />
+				{/* <MoreVertical className="button-hover" /> */}
+				<DropdownMenu trigger={<MoreVertical className="button-hover" />}>
+					{items.map((item, index) => (
+						<button
+							key={index}
+							className="hover:bg-gray-200 text-left text-sm px-4 py-1 cursor-pointer "
+							onClick={item.onClick}
+						>
+							{item.label}
+						</button>
+					))}
+				</DropdownMenu>
 			</footer>
 		</div>
 	);
